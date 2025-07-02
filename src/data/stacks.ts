21b78567e4a1e03a79e4523314b0df2f11938d90
Ezequiel Chorolque <ezequielchorolque14@gmail.com>
2025-07-02 16:30:56 -0300

    add icons and update stack

--- src/data/stacks.ts
+++ src/data/stacks.ts
@@ -1,10 +1,8 @@
 import IconAngular from "../components/icons/stack/IconAngular.astro";
 import IconJavascript from "../components/icons/stack/IconJavascript.astro";
-import IconHtml from "../components/icons/stack/IconHtml.astro";
 import IconTailwind from "../components/icons/stack/IconTailwind.astro";
 import IconBootstrap from "../components/icons/stack/IconBootstrap.astro";
 import IconTypescript from "../components/icons/stack/IconTypescript.astro";
-import IconCss from "../components/icons/stack/IconCss.astro";
 import IconAstro from "../components/icons/stack/IconAstro.astro";
 import IconNpm from "../components/icons/stack/IconNpm.astro";
 import IconRxjs from "../components/icons/stack/IconRxjs.astro";
@@ -12,27 +10,26 @@ import IconDocker from "../components/icons/stack/IconDocker.astro";
 import IconGit from "../components/icons/stack/IconGit.astro";
 import IconNotion from "../components/icons/stack/IconNotion.astro";
 import IconFirebase from "../components/icons/stack/IconFirebase.astro";
-import IconNetlify from "../components/icons/stack/IconNetlify.astro";
+import IconJasmine from "../components/icons/stack/IconJasmine.astro";
+import IconAzuredevops from "../components/icons/stack/IconAzuredevops.astro";
 
 export const stacks = {
-    frontend: [
+    "Desarrollo web": [
         { name: "Angular", icon: IconAngular },
         { name: "Astro", icon: IconAstro },
         { name: "TypeScript", icon: IconTypescript },
         { name: "JavaScript", icon: IconJavascript },
-        { name: "CSS", icon: IconCss },
-        { name: "HTML", icon: IconHtml },
-    ],
-    herramientas: [
         { name: "RxJS", icon: IconRxjs },
-        { name: "NgRx", icon: IconBootstrap },
+    ],
+    "Herramientas de desarrollo": [
         { name: "Bootstrap", icon: IconBootstrap },
         { name: "Tailwind", icon: IconTailwind },
+        { name: "Jasmine", icon: IconJasmine },
         { name: "NPM", icon: IconNpm },
     ],
-    conocimientos: [
-        { name: "Netlify", icon: IconNetlify },
+    "Tecnologías complementarias": [
         { name: "Docker", icon: IconDocker },
+        { name: 'Azure DevOps', icon: IconAzuredevops },
         { name: "Firebase", icon: IconFirebase },
         { name: "Git", icon: IconGit },
         { name: "Notion", icon: IconNotion },
