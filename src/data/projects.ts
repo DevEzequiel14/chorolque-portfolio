d7e6e1a37b2d9167da864baaadb67bc0bd09baa8
Ezequiel Chorolque <ezequielchorolque14@gmail.com>
2025-07-08 17:09:00 -0300

    update of technologies, descriptions and project context

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -6,16 +6,12 @@ export const projects = [
         image: "/projects/valorAr.webp",
         company: "Valor Ar",
         rol: "Desarrollador",
-        technologies: ["Angular", "Next.js", "Styled Components"],
-        timeline: "2023 - 2023",
-        description: `Esta es una pequeña descripción.De lo que fue
-        desarrollado el siguiente proyecto en cual se trabajo con React y 
-        Next.js, con Styled Components y con Typescript.`,
-        context: `Esta es una pequeña descripción.De lo que fue
-        desarrollado el siguiente proyecto en cual se trabajo con React y 
-        Next.js, con Styled Components y con Typescript.`,
+        technologies: ["Angular v17", "TypeScript", "SCSS", "Chart.js", "Netlify"],
+        timeline: "2024 - 2025",
+        description: `Visualización financiera interactiva sobre dólares, inflación y rendimiento de monedas en Argentina.`,
+        context: `ValorAr es una aplicación web pensada para quienes necesitan acceder rápidamente a información económica actualizada y fácil de interpretar. Desarrollada con Angular 17 y gráficos dinámicos con Chart.js, permite comparar valores de distintos tipos de dólar, analizar la evolución de la inflación, consultar tasas de plazos fijos en diferentes bancos y conocer el rendimiento de monedas digitales según entidad. Todo el contenido está organizado en vistas intuitivas, con filtros inteligentes que permiten seleccionar años, monedas o bancos. Fue publicada en Netlify y desarrollada como proyecto personal para reforzar habilidades en visualización de datos, experiencia de usuario y Angular moderno.`,
         link: "https://valorar.netlify.app/",
-        github: "https://github.com/DevEzequiel14/ValorAr",
+        github: "https://github.com/DevEzequiel14/ValorAr"
     },
     {
         title: "Quincho el Tata",
@@ -24,14 +20,11 @@ export const projects = [
         image: "/projects/quinchoElTata.webp",
         company: "Quincho el Tata",
         rol: "Desarrollador Frontend",
-        technologies: ["React", "Next.js", "Styled Components"],
-        timeline: "2021 - 2025",
-        description: `Esta es una pequeña descripción.De lo que fue
-        desarrollado el siguiente proyecto en cual se trabajo con React y Next.js, con Styled Components y con Typescript.`,
-        context: `Esta es una pequeña descripción.De lo que fue
-        desarrollado el siguiente proyecto en cual se trabajo con React y 
-        Next.js, con Styled Components y con Typescript.`,
+        technologies: ["Angular v19", "Bootstrap", "TypeScript", "Netlify"],
+        timeline: "2025",
+        description: `Landing page optimizada para la promoción de un espacio de eventos en Jujuy.`,
+        context: `Quincho El Tata es una landing page desarrollada para brindar presencia digital a un espacio físico de eventos. Está pensada para facilitar el contacto rápido y directo con el dueño a través de WhatsApp, e informar de forma clara sobre los servicios disponibles. Incluye secciones como "Nosotros", con detalles del lugar, horarios, capacidad y una imagen del quincho; "Servicios", donde se listan las comodidades principales y adicionales; y "Contacto", con acceso a redes sociales, mapa de ubicación, y datos clave. Fue construida con Angular 19 y Bootstrap, priorizando estructura clara, carga rápida y navegación simple desde cualquier dispositivo.`,
         link: "https://quinchoeltata.netlify.app/",
-        github: "https://github.com/DevEzequiel14/Quincho-El-Tata",
-    },
+        github: "https://github.com/DevEzequiel14/Quincho-El-Tata"
+    }
 ];
